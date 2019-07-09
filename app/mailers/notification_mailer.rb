a5e5410cfeed3a5883a6a2d36c3d04f00f484c0d
Devon Proudfoot <proudfoot.devon@gmail.com>
2019-07-09 23:12:29 +0000

    updated website with gmail login for sending out emails

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -2,6 +2,6 @@ class NotificationMailer < ApplicationMailer
   default from: "no-reply@nomsterapp.com"
 
   def comment_added
-    mail(to: "my@email.com", subject: "A comment has been added to your court!")
+    mail(to: "proudfoot.devon@gmail.com", subject: "A comment has been added to your court!")
   end
 end
