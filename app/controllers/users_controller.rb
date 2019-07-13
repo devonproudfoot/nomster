c30f9f7a00c3a177463b2c22f29d7424446babe2
Devon Proudfoot <proudfoot.devon@gmail.com>
2019-07-13 19:47:20 +0000

    Only a user can see thier user page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,12 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
 
   def show
     @user = User.find(params[:id])
+
+    if @user != current_user
+      return render plain: 'Not allowed', status: :forbidden
+    end
   end
 
 end
